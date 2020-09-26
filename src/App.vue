e6aab136b777e91cf68b570023faa6069e3f82c0
ferryal <ferryal.bahari@amartha.com>
2020-09-26 10:19:37 +0700

    initial commit: setup webpack & babel

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// import './styles/App.styl'
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="css">
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+  background-color: #FFFAE6;
+}
+</style>
